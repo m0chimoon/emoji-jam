306189d2b14c25edb8ac09ce006b84623cbeaa95
m0chi/ <aly0mal99@gmail.com>
2024-06-12 12:16:01 +0100

    project "Emoji Jam"

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="flex flex-col items-center gap-6 mt-16 mb-10 font-nunito font-semibold">
-      <h1 className="text-7xl uppercase">😇Emojam😈</h1>
+      <h1 className="text-7xl uppercase">😇Emoji Jam😈</h1>
     </header>
   )
 }
